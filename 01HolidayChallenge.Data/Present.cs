aab5a2bda0bff916e9024987865049f3e4f2771b
Jacquelyn Mitchell <jac.nmitchell@gmail.com>
2021-12-23 09:31:14 -0500

    Added classes to Data Layer

---
+++ 01HolidayChallenge.Data/Present.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01HolidayChallenge.Data
+{
+    public class Present
+    {
+        public int Id { get; set; }
+        public enum PresentType { }
+        public string Wrapping { get; set; }
+        public string Contains { get; set; }
+        public List<string> Hints { get; set; }
+        public int TimesShaken { get; set; }
+        public bool IsWrapped { get; set; }
+        public int TreeId { get; set; }
+        public virtual Tree Tree { get; set; }
+    }
+}
---
+++ 01HolidayChallenge.Data/Tree.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01HolidayChallenge.Data
+{
+    public class Tree
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public virtual List<Ornament> Ornaments { get; set; }
+    }
+}
